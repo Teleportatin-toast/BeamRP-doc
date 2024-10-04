8e39ec8ea5a04dcfd067f8c9676d1b6ba445a3ae
Teleportatin-toast <tele.toast@proton.me>
2024-10-04 00:45:32 -0400

    Just work please...

--- javascript.js
+++ javascript.js
@@ -1,18 +1,9 @@
-function load(url) {
-    // display loading image here...
-    document.getElementById('loadingImg').visible = true;
-    // request your data...
-    var req = new XMLHttpRequest();
-    req.open("POST", url, true);
-
-    req.onreadystatechange = function () {
-        if (req.readyState == 4 && req.status == 200) {
-            // content is loaded...hide the gif and display the content...
-            if (req.responseText) {
-                document.getElementById('content').innerHTML = req.responseText;
-                document.getElementById('loadingImg').visible = false;
-            }
-        }
-    };
-    request.send(vars);
-}
+document.onreadystatechange = function() {
+    if (document.readyState !== "complete") {
+        document.querySelector("body").style.visibility = "hidden";
+        document.querySelector("#loader").style.visibility = "visible";
+    } else {
+        document.querySelector("#loader").style.display = "none";
+        document.querySelector("body").style.visibility = "visible";
+    }
+};
