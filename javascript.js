9c20631deff981c33c9d82f30145779a2c9e4757
Teleportatin-toast <tele.toast@proton.me>
2024-10-08 18:57:50 -0400

    Added the rest of the jobs (exc. business). Page links now should work between github and live server. Small grammar corrections.

--- javascript.js
+++ javascript.js
@@ -5,3 +5,26 @@ setTimeout(function() {
         preCon.remove();
     }
 }, 400);
+
+// Link backup (Best to delete in final page (I prorbably will forget to do this though))
+function checkLink(docName) {
+    var firstLink = docName;
+    var secondLink = docName + '.html';
+  
+    var xhr = new XMLHttpRequest();
+    xhr.open('HEAD', firstLink, true);
+  
+    xhr.onload = function() {
+      if (xhr.status === 200) {
+        window.location.href = firstLink;
+      } else {
+        window.location.href = secondLink;
+      }
+    };
+  
+    xhr.onerror = function() {
+      window.location.href = secondLink;
+    };
+  
+    xhr.send();
+  }
